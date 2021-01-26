c73dc69812e6b04158481bb6e4cb488b5636d4fa
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-01-25 21:03:31 -0300

    H2 database

---
+++ backend/src/main/java/com/charlistonrodrigo/dscatalog/services/CategoryService.java
@@ -0,0 +1,21 @@
+package com.charlistonrodrigo.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.charlistonrodrigo.dscatalog.entities.Category;
+import com.charlistonrodrigo.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+
+}
