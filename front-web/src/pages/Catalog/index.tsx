64d33d2c9a5c910da4ff226412d2fef9cfbd6747
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-01-31 19:47:13 -0300

    Finish catalog loaders

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ProductsResponse } from '../../core/types/Product';
-import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from 'core/types/Product';
+import { makeRequest } from 'core/utils/request';
 import ProductCard from './components/ProductCard';
+import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 
 const Catalog = () =>{
 //Quando a lista de produtos estiver disponivel, 
 //popular um estado no componente, e listar os produtos dinâmicamente.
 const [productsResponse, setProductsResponse] = useState<ProductsResponse>(); 
-
-console.log(productsResponse);
+const [isLoading, setIsLoading] = useState(false);
 //Quando o componente iniciar, buscar a lista de produtos.
 useEffect(() =>{
     
@@ -18,9 +18,14 @@ useEffect(() =>{
         page: 0,
         linePerPage: 12
     }
-
+    // iniciar o loader
+    setIsLoading(true);
     makeRequest({ url: '/products', params })
-        .then(response => setProductsResponse(response.data));
+        .then(response => setProductsResponse(response.data))
+        .finally(() => {
+        // finalizar o loader
+        setIsLoading(false);
+        })
 }, []);
 
     return (
@@ -29,11 +34,14 @@ useEffect(() =>{
                 Catálogo de produtos
             </h1>
             <div className="catalog-products">
-               {productsResponse?.content.map(product => (
-                   <Link to={`/products/${product.id}`} key={product.id}>
-                       <ProductCard product={product}/>
-                   </Link>
-               ))}        
+             { isLoading ? <ProductCardLoader /> : (
+                productsResponse?.content.map(product => (
+                    <Link to={`/products/${product.id}`} key={product.id}>
+                        <ProductCard product={product}/>
+                    </Link>
+               ))
+             )}
+                       
             </div>
         </div>
      );
