58b4bcebf5195bfa58dc4d303160b732e900aba3
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-02-09 22:15:45 -0300

    User CRUD

---
+++ backend/src/main/java/com/charlistonrodrigo/dscatalog/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.charlistonrodrigo.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.charlistonrodrigo.dscatalog.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
