ebebb24107e66f6f833ba32cefda4430087517ad
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-02-10 18:46:39 -0300

    Basic beans validation

--- backend/src/main/java/com/charlistonrodrigo/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/charlistonrodrigo/dscatalog/dto/ProductDTO.java
@@ -6,6 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.charlistonrodrigo.dscatalog.entities.Category;
 import com.charlistonrodrigo.dscatalog.entities.Product;
 
@@ -15,10 +20,19 @@ public class ProductDTO implements Serializable{
 
 
 	private Long id;
+	
+	@Size(min = 5, max = 60, message = "Deve ter entre 5 e 60 caracteres")
+	@NotBlank(message = "Campo requirido")
 	private String name;
+	
+	@NotBlank(message = "Campo requirido")
 	private String description;
+	
+	@Positive(message = "O preço deve ser um valor positivo")
 	private double price;
 	private String imgUrl;
+	
+	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 	
 	
