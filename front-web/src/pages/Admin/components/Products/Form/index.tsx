3b075794e21d8d45ff6f053fba2bce187d3baff9
charlistonrodrigo <charlistonrodrigo75@gmail.com>
2021-09-26 00:12:48 -0300

    Apply login form validations

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,4 +1,4 @@
-import { makeRequest } from 'core/utils/request';
+import { makePrivateRequest } from 'core/utils/request';
 import React, { useState } from 'react';
 import BaseForm from '../../BaseForm';
 import  './styles.scss';
@@ -34,7 +34,7 @@ import  './styles.scss';
              imgUrl: 'https://cdn.awsli.com.br/800x800/259/259434/produto/54859844/dd327c1b46.jpg',
              categories: [{ id: formData.category }]
          }
-         makeRequest({ url: '/products', method: 'POST', data: payload })
+         makePrivateRequest({ url: '/products', method: 'POST', data: payload })
            .then(() => {
                 setFormData({ name:'', category:'', price: '', description:''});
            });
